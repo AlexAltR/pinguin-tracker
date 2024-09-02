abc9fbb3df8e020aae7660e1f1238734c6d8ebbd
AlexAltR <trud.resurs.altay@gmail.com>
2024-09-02 21:42:55 +0600

    Project 'penguin-tracker' v1.0

--- models/db.js
+++ models/db.js
@@ -4,7 +4,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'penguin_tracker'
+    database: process.env.DB_NAME || 'penguin_tracker',
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
